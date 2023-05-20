241651fb1fd38ae37e67759444c23e5e1bcf01f1
Hyune-c <choi8608@gmail.com>
2023-05-21 05:07:54 +0900

    project init

---
+++ src/main/kotlin/com/example/vouchersystem/VoucherSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.example.vouchersystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VoucherSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<VoucherSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/vouchersystem/VoucherSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.vouchersystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VoucherSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
