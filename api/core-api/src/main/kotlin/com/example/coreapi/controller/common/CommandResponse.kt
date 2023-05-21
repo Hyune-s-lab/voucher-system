917ba6a29e8e7763bad6888aa02af5779cad8fa7
Hyune-c <choi8608@gmail.com>
2023-05-21 22:14:41 +0900

    [m2] api 구현 - common request, response 구현

---
+++ api/core-api/src/main/kotlin/com/example/coreapi/controller/common/CommandResponse.kt
@@ -0,0 +1,9 @@
+package com.example.coreapi.controller.common
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(title = "command api 필수 응답 값")
+interface CommandResponse {
+    @get:Schema(title = "거래고유번호")
+    val tid: String
+}
---
+++ api/core-api/src/main/kotlin/com/example/coreapi/controller/common/QueryRequest.kt
@@ -0,0 +1,6 @@
+package com.example.coreapi.controller.common
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(title = "query api 필수 요청 값")
+interface QueryRequest
